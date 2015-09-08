c584a495ab6a466e08db224910918b11540338c1
Steve McCann <steven.r.mccann@gmail.com>
2015-09-07 22:23:59 -0400

    refactor: switch MovieDetailActivity to AppCompatActivity
    Per code review recommendation, changing the class the activity extends to AppCompatActivity

--- app/src/main/java/net/stevemccan/android/popularmovies/MovieDetailActivity.java
+++ app/src/main/java/net/stevemccan/android/popularmovies/MovieDetailActivity.java
@@ -1,7 +1,7 @@
 package net.stevemccan.android.popularmovies;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 
-public class MovieDetailActivity extends ActionBarActivity {
+public class MovieDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
